33d640624ef3a721fdbfbba8edd25004dc13526a
Alwin <alwin@helloastral.com>
2025-03-11 08:10:03 +0530

    imports Room model in rooms module

--- src/modules/rooms/rooms.module.ts
+++ src/modules/rooms/rooms.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
+import { Room } from './rooms.model';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
+  imports: [TypegooseModule.forFeature([Room])],
   controllers: [RoomsController],
   providers: [RoomsService],
 })
