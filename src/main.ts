376ba3a6c5391f7050f7f8b2322bdf3fa7fddaf8
Alwin <alwin@helloastral.com>
2025-03-16 16:50:33 +0530

    fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const PORT = process.env.PORT || 3000;
   const HOST = process.env.HOST || 'localhost';
