d0a1223dddc1deb8f0a9c13fec451870575f2a84
Alwin <alwin@helloastral.com>
2025-03-22 15:53:59 +0530

    Adds change password endpoint in user controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -7,9 +7,12 @@ import {
   Param,
   Delete,
   NotFoundException,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.model';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -30,6 +33,15 @@ export class UsersController {
     const user = await this.usersService.findOne(id);
     if (!user) throw new NotFoundException('User not found');
     return user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('/change-password')
+  async changePassword(
+    @Body() changePasswordDto: { oldPassword: string; newPassword: string },
+    @Req() req,
+  ) {
+    return this.usersService.changePassword(req.user.userId, changePasswordDto);
   }
 
   @Patch(':id')
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { User } from './user.model';
 import { ReturnModelType } from '@typegoose/typegoose';
 import * as bcryptjs from 'bcryptjs';
 
+const salt = bcryptjs.genSaltSync(10);
 @Injectable()
 export class UsersService {
   constructor(
@@ -11,7 +12,6 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: User) {
-    const salt = bcryptjs.genSaltSync(10);
     const encrypted = bcryptjs.hashSync(createUserDto.password, salt);
 
     const createdUser = new this.userModel({
@@ -32,6 +32,25 @@ export class UsersService {
 
   async findOneByEmail(email: string) {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  async changePassword(
+    userId: string,
+    { newPassword, oldPassword }: { oldPassword: string; newPassword: string },
+  ) {
+    const user = await this.userModel.findById(userId).exec();
+    const oldPasswordMatched = bcryptjs.compare(oldPassword, user.password);
+    if (!oldPasswordMatched) {
+      throw new BadRequestException('Password does not match');
+    }
+    const newEncryptedPassword = bcryptjs.hashSync(newPassword, salt);
+
+    const updatedUser = await user
+      .updateOne({
+        password: newEncryptedPassword,
+      })
+      .exec();
+    return updatedUser;
   }
 
   async update(id: string, updateUserDto: Partial<User>) {
